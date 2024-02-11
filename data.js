9c0dc0e8c8cf50364c4df8759454d93e7cd655e8
Alcario <albano_caminos@hotmail.com>
2024-02-11 17:43:57 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,19 @@
+const libros = [
+    {
+        id: 1,
+        titulo: 'El principito',
+        autor: 'Antoine de Saint-Exupéry'
+    },
+    {
+        id: 2,
+        titulo: 'El quijote de la mancha',
+        autor: 'Miguel de Cervantes'
+    },
+    {
+        id: 3,
+        titulo: 'Don Quijote de la Mancha',
+        autor: 'Miguel de Cervantes'
+    }
+];
+
+module.exports = libros;
